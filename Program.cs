a17f15e556fac155c19688b4aa674497e33cb58b
martincudicio <martin.cudicio@edu.devinci.fr>
2019-11-12 22:37:11 +0100

    change the Hello World

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Monopoly_game
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World MArtin !!!");
         }
     }
 }
