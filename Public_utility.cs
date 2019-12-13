6311e724b34ec251dd90612f2f52a5423536e73a
jdelebec <delebecquejeanlouis@gmail.com>
2019-12-13 22:29:14 +0100

    remise a niveau après 8000 conneries avec git

--- Public_utility.cs
+++ Public_utility.cs
@@ -10,14 +10,16 @@ namespace Monopoly_game
     {
         private int buying_prize;
         private int rent;
-
-        public Public_utility(int buying_prize,int rent, string name, string display) : base(name, display)
+        private int mortgage;
+        public Public_utility(int buying_prize,int rent,int mortgage, string name, string display) : base(name, display)
         {
             this.Buying_prize = buying_prize;
             this.Rent = rent;
+            this.mortgage = mortgage;
         }
 
         public int Buying_prize { get => buying_prize; set => buying_prize = value; }
         public int Rent { get => rent; set => rent = value; }
+        public int Mortgage { get => mortgage; set => mortgage = value; }
     }
 }
--- gare_station.cs
+++ gare_station.cs
@@ -9,12 +9,15 @@ namespace Monopoly_game
     class gare_station : Box
     {
         private int buying_prize;
+        private int mortgage;
 
-        public gare_station(int buying_prize, string name,string display) : base (name,display)
+        public gare_station(int buying_prize,int mortgage, string name,string display) : base (name,display)
         {
             this.Buying_prize = buying_prize;
+            this.Mortgage = mortgage;
         }
 
         public int Buying_prize { get => buying_prize; set => buying_prize = value; }
+        public int Mortgage { get => mortgage; set => mortgage = value; }
     }
 }
